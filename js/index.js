3bf01198c6363353ab1f0a8a513fc31b377702c3
RDprogramming <rdprogramming24@gmail.com>
2024-05-22 18:02:54 +0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function toggleMoreText() {
 
     if (moreText.style.display === "none") {
       moreText.style.display = "block";
-      btnText.innerHTML = "Read Less";
+      btnText.innerHTML = "Թաքցնել";
     } else {
       moreText.style.display = "none";
       btnText.innerHTML = "Read More";
